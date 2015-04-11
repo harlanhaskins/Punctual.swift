0ccb151632534ff16d5d8b7be8e9209c618f5b64
Harlan Haskins <harlan@harlanhaskins.com>
2015-04-11 17:43:00 -0400

    Removed didReceiveMemoryWarning

--- PunctualExample/PunctualExample/DayCalculatorViewController.swift
+++ PunctualExample/PunctualExample/DayCalculatorViewController.swift
@@ -16,11 +16,6 @@ class DayCalculatorViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func updateDate() {
         if let
