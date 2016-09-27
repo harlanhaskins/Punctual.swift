fba4b1d985709486f4f6086fabdcc367cd310594
Siemen Sikkema <siemen@unclutter.nl>
2016-09-27 13:35:58 +0200

    Remove unused dependency SwiftCheck

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "Punctual",
-    dependencies: [
-      .Package(url: "https://github.com/typelift/SwiftCheck.git", versions: Version(0,6,0)..<Version(1,0,0))
-    ]
+    name: "Punctual"
 )
